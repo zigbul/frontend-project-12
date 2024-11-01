b1531a74ef672a80fd3078cdcebe05af58f537b7
zigbul <65498093+zigbul@users.noreply.github.com>
2024-11-02 00:08:16 +0300

    add formik

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,5 +1,18 @@
+import { Formik, Form, Field } from 'formik';
+
 const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <>
+      Войти
+      <Formik>
+        <Form>
+          <Field type="nickname" name="nickname" placeholder="Ваш ник" />
+          <Field type="password" name="password" placeholder="Пароль" />
+          <button type="submit">Войти</button>
+        </Form>
+      </Formik>
+    </>
+  );
 };
 
 export default LoginPage;
